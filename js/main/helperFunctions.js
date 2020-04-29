a2716e18194f340db8eac3ccebe2fa71f139a67b
donimuvukor <40676968+donimuvukor@users.noreply.github.com>
2020-04-29 17:15:55 +0100

    Update helperFunctions.js
    Adds to query string and modifies console.log for var data

--- js/main/helperFunctions.js
+++ js/main/helperFunctions.js
@@ -38,7 +38,7 @@ fetch(country_url+api_key)
     let out_amount = document.getElementById('out_amount');
     let from_curr = document.getElementById('from_curr');
     let to_curr = document.getElementById('to_curr');
-    query = from_curr.value+'_'+to_curr.value; 
+    query = from_curr.value+'_'+to_curr.value+'_'+to_curr.value+'_'+from_curr.value; 
     let url = rate_url+query+query_params+api_key;
     fetch(url)
         .then(response => response.json())
@@ -48,7 +48,7 @@ fetch(country_url+api_key)
             for(let r in rates){
                 ex_rate.push(rates[r].val);//Store exchange rate pairs
             }
-            console.log(data);
+            console.log(data.results);
             console.log(ex_rate[0]);
             console.log(in_amount.value);
             console.log(ex_rate[0]*in_amount.value);
